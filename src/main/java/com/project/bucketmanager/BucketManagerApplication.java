f86fa3398ee7c7bf6843f5e54791c7a707331ed2
Paulo Neves <pneves@saqja.com.br>
2023-06-19 15:24:57 -0300

    swagger

--- src/main/java/com/project/bucketmanager/BucketManagerApplication.java
+++ src/main/java/com/project/bucketmanager/BucketManagerApplication.java
@@ -3,13 +3,24 @@ package com.project.bucketmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableCaching
+@EnableSwagger2
 public class BucketManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BucketManagerApplication.class, args);
+    }
+    public Docket apis(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.project.bucketmanager.Controllers"))
+                .build();
     }
 
 }
