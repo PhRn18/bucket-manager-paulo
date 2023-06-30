af0288500acbcdb29aaed6ad1d021d65a3a0ed22
Paulo Neves <pneves@saqja.com.br>
2023-06-30 14:15:39 -0300

    auto-create-buckets feature

---
+++ src/main/java/com/project/bucketmanager/Config/AutoCreateBuckets.java
@@ -0,0 +1,49 @@
+package com.project.bucketmanager.Config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Configuration
+public class AutoCreateBuckets {
+
+    private static final Logger logger = LoggerFactory.getLogger(AutoCreateBuckets.class);
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private S3Client s3Client;
+
+    @PostConstruct
+    @ConditionalOnProperty(value = "aws.enable.auto-create-buckets", havingValue = "true")
+    public void createBuckets(){
+        String[] names = environment.getRequiredProperty("aws.bucket.names", String[].class);
+
+        ListBucketsRequest listBucketsRequest = ListBucketsRequest.builder().build();
+        List<Bucket> buckets = s3Client.listBuckets(listBucketsRequest).buckets();
+
+        for (String name : names) {
+            boolean bucketAlreadyExists = !buckets.isEmpty() && buckets.stream().anyMatch(bucket -> bucket.name().equals(name));
+            if (bucketAlreadyExists) {
+                logger.warn("Bucket already exists: " + name);
+            } else {
+
+                CreateBucketRequest createBucketRequest = CreateBucketRequest.builder()
+                        .bucket(name)
+                        .build();
+
+                CreateBucketResponse createBucketResponse = s3Client.createBucket(createBucketRequest);
+                logger.info("Bucket created: " + createBucketResponse.location());
+            }
+        }
+    }
+}
