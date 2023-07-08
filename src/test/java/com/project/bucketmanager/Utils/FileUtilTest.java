0bccceae694ded7eab2f85a4d8372b9f5dd0e152
Paulo Neves <pneves@saqja.com.br>
2023-07-08 15:18:15 -0300

    writing FileUtil tests

---
+++ src/test/java/com/project/bucketmanager/Utils/FileUtilTest.java
@@ -0,0 +1,42 @@
+package com.project.bucketmanager.Utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class FileUtilTest {
+
+    @Test
+    void getFolderFromKey() {
+        String key = "c/folder/result.txt";
+        String folder = FileUtil.getFolderFromKey(key);
+        assertThat(folder).isNotNull();
+        assertThat(folder).isEqualTo("c/folder");
+    }
+
+    @Test
+    void getExtensionFromKey() {
+        String key = "c/folder/result.txt";
+        String extension = FileUtil.getExtensionFromKey(key);
+        assertThat(extension).isNotNull();
+        assertThat(extension).isEqualTo("txt");
+    }
+
+    @Test
+    void getFileNameWithoutExtension() {
+        String key = "result.txt";
+        String fileName = FileUtil.getFileNameWithoutExtension(key);
+        assertThat(fileName).isNotNull();
+        assertThat(fileName).isEqualTo("result");
+    }
+
+    @Test
+    void getOriginalFileExtension() {
+        String key = "result-txt.gz";
+        String originalFileExtension = FileUtil.getOriginalFileExtension(key);
+        assertThat(originalFileExtension).isNotNull();
+        assertThat(originalFileExtension).isEqualTo("txt");
+    }
+}
