b6d0f2b06e8fbb9b6d856965c1ce2eaee2d73172
Paulo Neves <pneves@saqja.com.br>
2023-07-09 13:11:27 -0300

    writing GzipUtilsTest

---
+++ src/test/java/com/project/bucketmanager/Utils/GzipUtilsTest.java
@@ -0,0 +1,25 @@
+package com.project.bucketmanager.Utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.util.StreamUtils;
+
+import java.io.ByteArrayInputStream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class GzipUtilsTest {
+    @Test
+    void getCompressedBytesUsingGZIP() {
+        String content = "mock-content";
+        byte[] contentBytes = content.getBytes();
+        MockMultipartFile multipartFile = new MockMultipartFile("file.txt", contentBytes);
+
+        byte[] compressedBytes = assertDoesNotThrow(() ->
+                GzipUtils.getCompressedBytesUsingGZIP(multipartFile.getInputStream()));
+
+        assertThat(compressedBytes).isNotEmpty();
+    }
+}
