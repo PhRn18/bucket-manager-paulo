7471b521fcd8c3fe1ef693a06e65f421e6698158
Paulo Neves <pneves@saqja.com.br>
2023-07-11 11:56:04 -0300

    initializing security

---
+++ src/main/java/com/project/bucketmanager/Config/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.project.bucketmanager.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return  httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .antMatchers(HttpMethod.GET, "/healthy").permitAll()
+                        .antMatchers(HttpMethod.GET, "/token").permitAll()
+                        .antMatchers(HttpMethod.GET,"/bucket/list").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+}
